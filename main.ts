fd1c5c3819d78bd027f84e53426312769ebf97d6
elpinfloisumare <112429602+elpinfloisumare@users.noreply.github.com>
2022-09-01 11:47:54 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString(texto)
+})
+let texto = ""
+texto = "AAAA"
+basic.forever(function () {
+	
+})
